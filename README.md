99738c1c99aa8a65b50e8149ae0514198dd70442
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2023-10-16 21:12:37 +0200

    Many files with flaws :)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # demo-megalinter
+
 Demo repository for MegaLinter
+
+[Oh this is a dead link !](https://this.is.a.dead.link.com)
---
+++ src/typescript_good_1.ts
@@ -0,0 +1,6 @@
+const spiderman = (person: string): string => {
+  return 'Hello, ' + person
+}
+
+const user = 'Peter Parker'
+console.log(spiderman(user))
